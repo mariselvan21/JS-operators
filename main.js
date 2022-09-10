b5c78ff419f3cb6ec42088290201565866056f87
c.mariselvan <mari20041999@gmail.com>
2022-09-10 13:49:15 +0530

    add files

---
+++ main.js
@@ -0,0 +1,140 @@
+// Arithmetic operators
+//Addition operator
+var a=2;
+var b=5;
+var c=a+b;
+console.log("Addition operator");
+console.log(c);
+//Multiplication operator
+var a=4;
+var b=10;
+var c=a*b;
+console.log("Multiplication operator");
+console.log(c);
+//subtraction operator
+var a=10;
+var b=5;
+var c=a-b;
+console.log("subtraction operator");
+console.log(c);
+//division operator
+var a=10;
+var b=5;
+var c=a/b;
+console.log("division operator");
+console.log(c);
+// Exponention operator
+var a=5;
+var b=2;
+var c=a**b;
+console.log("Exponention operator");
+console.log(c);
+// Modulus operator
+var a=3;
+var b=4;
+var c=a%b;
+console.log("Modulus operator");
+console.log(c);
+// Increment operator
+var a=5;
+a++;
+console.log("Increment operator");
+console.log(a);
+// Decrement operator
+var a=5;
+a--;
+console.log("Decrement operator");
+console.log(a);
+// Java script assignment operator
+var x=10;
+console.log("value assign to variable");
+console.log(x);
+
+console.log("value assign to another variable");
+var x=10;
+var y=5;
+x=y;
+console.log(x);
+
+console.log("Adding value save same variable");
+var x=10;
+var y=5;
+x+=y;
+console.log(x);
+
+console.log("Subtraction value save same variable");
+var x=10;
+var y=5;
+x-=y;
+console.log(x);
+
+console.log("Multiplication value save same variable");
+var x=10;
+var y=5;
+x*=y;
+console.log(x);
+
+console.log("Division value save same variable");
+var x=10;
+var y=5;
+x/=y;
+console.log(x);
+
+console.log("Modulo value save same variable");
+var x=10;
+var y=5;
+x%=y;
+console.log(x);
+
+console.log("Exponention value save same variable");
+var x=10;
+var y=5;
+x**=y;
+console.log(x);
+
+console.log("Java script string operator");
+
+console.log("Adding string operator");
+var x="mari";
+var y="selvam";
+x+=y;
+console.log(x);
+
+console.log("Adding string and number");
+var x="mari";
+var y=5;
+x+=y;
+console.log(x);
+
+console.log("Adding string and two numbers");
+var x="mari";
+var y=5;
+var z=10;
+var w=x+y+z;
+console.log(w);
+
+console.log("Adding string and number");
+var x=5;
+var y=20;
+var z="mari";
+var w=x+y+z;
+console.log(w);
+
+
+
+// ternary operator
+var age=17;
+var eligible=(age<18)? "Too young":"old enough";
+console.log(eligible);
+
+
+
+
+
+
+
+
+
+
+
+
